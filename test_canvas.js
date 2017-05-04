56b836d83b1be8ccd8f3620604803eb4f9aca6fc
Mike Newton <mikenewton@Mac-mini.local>
2017-05-04 11:38:41 -0400

    Test more defaults

--- test_canvas.js
+++ test_canvas.js
@@ -5,6 +5,15 @@ canvasEl.width = window.innerWidth / 2;
 
 const ctx = canvasEl.getContext('2d');
 
+// load default image
+    let defaultImg = new Image();
+    defaultImg.src = './defaults/rgb_default.png';
+    defaultImg.onload = function(){
+        canvasEl.width = defaultImg.width / 2;
+        canvasEl.height = defaultImg.height / 2;
+        ctx.drawImage(defaultImg,0,0,defaultImg.width / 2,defaultImg.height / 2);
+    }
+
 // no reason this couldn't work with multiple files, right?
 let defaultHowl1 = new Howl({
   preload: true,
@@ -21,8 +30,40 @@ let defaultHowl1 = new Howl({
   }
 });
 
+let defaultHowl2 = new Howl({
+  preload: true,
+  volume: 0.5,
+  src: './defaults/80s_vibe.mp3',
+  onload: function() {
+    console.log('Loaded Default 2!');
+  },
+  onplay: function() {
+    console.log('Playing Default 2!');
+  },
+  onend: function() {
+    console.log('Finished Default 2!');
+  }
+});
+
+let defaultHowl3 = new Howl({
+  preload: true,
+  volume: 0.5,
+  src: './defaults/rave_digger.mp3',
+  onload: function() {
+    console.log('Loaded Default 3!');
+  },
+  onplay: function() {
+    console.log('Playing Default 3!');
+  },
+  onend: function() {
+    console.log('Finished Default 3!');
+  }
+});
+
 let soundObj = {};
 soundObj['audio1'] = defaultHowl1;
+soundObj['audio2'] = defaultHowl2;
+soundObj['audio3'] = defaultHowl3;
 
 
 const imageLoader = document.getElementById('imageLoader');
