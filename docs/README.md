9e4bea2ef7d63061d4beaf965c82f4885e7b39a8
Mike Newton <mikenewton@Mac-mini.local>
2017-05-04 21:33:16 -0400

    Make Thursday night update

---
+++ docs/README.md
@@ -0,0 +1,55 @@
+## Hum
+
+### Background
+
+Hum will be a tool to mix sound via images: the RGB pixel data of a given image will modulate the volume of three sound files (one for each channel). My goal for this project is that it may be a fun and intuitive music-making tool for people without much musical experience (which would include myself).
+
+### MVP
+
+Users will be able to:
+- Upload image and sound files.
+- Play/pause/stop the music.
+- **Addition:** Draw colors to the canvas to affect the sound.
+
+The app will:
+- Modulate sound via an image's RGB color data.
+- Display uploaded images and play uploaded sound.
+- Display relevant file names
+
+There will also be:
+- A modal with information on how to use the app.
+- a production README.
+
+### Technologies
+
+This project will be implemented with:
+- `Howler.js` - a JS library for working with sound.
+- The `HTML5 Canvas` and `File` APIs (allows for uploading files without a backend).
+- The `Canvas`'s `getImageData()` function.
+- `Webpack` to bundle files.
+
+There will be individual scripts to handle distinct elements of the app: `display.js` for the main display, `sound_file.js` to handle individual sound files, and `modulate.js` to handle the actual work of modifying the sound files.
+
+### Wireframes
+
+The app will consist of a single screen (plus the instructions modal). There will be simple controls to upload files and to play/pause/stop the audio. There will also be links to my own sites (Github, LinkedIn, etc.). Most of the app will be given over to the uploaded image - in a sense this app can also function as a sort of gallery display for a given image.
+
+![image of main app](./hum_main_app.png)
+![image of instructions](./hum_instructions.png)
+
+### Implementation Timeline
+
+**Day 1:** Test out the basics of the technologies/libraries I'm implementing - make sure I'm able to upload files via the Canvas, play multiple audio files via Howler, etc. Also, work on overall visual layout.
+
+**Day 2:** Go further into these technologies - modulate audio files via Howler, get pixel data via the Canvas, etc.
+
+**Day 3:** Build out the core logic of the app - the goal is to be able to upload audio and image files and to gradually modulate the audio files based on the pixel data in individual images.
+
+**Day 4:** Add styling to the controls. Add the instructions modal, links, etc.
+
+### Bonus Features
+
+Once the basics are working, there are all sorts of ways this project could expand, including:
+- More controls for modulating the audio.
+- The ability to save files on a backend.
+- The ability to integrate live video imagery rather than just static image files.
--- test_canvas.js
+++ test_canvas.js
@@ -1,127 +1,138 @@
 
 const canvasEl = document.getElementsByTagName("canvas")[0];
-canvasEl.height = window.innerHeight / 2;
-canvasEl.width = window.innerWidth / 2;
+// canvasEl.height = window.innerHeight / 2;
+// canvasEl.width = window.innerWidth / 2;
 
 const ctx = canvasEl.getContext('2d');
 
-//load default image, set to canvas
-// BUT not working locally!
-// need to use File API here?
-let defaultImg = new Image();
-defaultImg.src = 'defaults/rgb_default.png';
- // defaultImg.setAttribute('crossOrigin', '');
-defaultImg.onload = function(){
-  canvasEl.width = defaultImg.width / 2;
-  canvasEl.height = defaultImg.height / 2;
-  ctx.drawImage(defaultImg,0,0,defaultImg.width / 2,defaultImg.height / 2);
-}
-
-// load default audio - seems to work fine with multiple files
-// BUT having errors locally!
-let defaultHowl1 = new Howl({
-  preload: true,
-  volume: 0.5,
-  src: 'defaults/neverturnback.mp3',
-  onload: function() {
-    console.log('Loaded Default 1!');
-  },
-  onplay: function() {
-    console.log('Playing Default 1!');
-  },
-  onend: function() {
-    console.log('Finished Default 1!');
-  }
-});
-
-let defaultHowl2 = new Howl({
-  preload: true,
-  volume: 0.5,
-  src: 'defaults/80s_vibe.mp3',
-  onload: function() {
-    console.log('Loaded Default 2!');
-  },
-  onplay: function() {
-    console.log('Playing Default 2!');
-  },
-  onend: function() {
-    console.log('Finished Default 2!');
-  }
-});
-
-let defaultHowl3 = new Howl({
-  preload: true,
-  volume: 0.5,
-  src: 'defaults/rave_digger.mp3',
-  onload: function() {
-    console.log('Loaded Default 3!');
-  },
-  onplay: function() {
-    console.log('Playing Default 3!');
-  },
-  onend: function() {
-    console.log('Finished Default 3!');
-  }
-});
-
 let soundObj = {};
-soundObj['audio1'] = defaultHowl1;
-soundObj['audio2'] = defaultHowl2;
-soundObj['audio3'] = defaultHowl3;
+
+let setInt;
+let current_x = 0;
+
+let currentImg;
 
 const colorInfoButton = document.getElementById('colorInfoButton');
-colorInfoButton.addEventListener('click', getColorInfo, false);
+colorInfoButton.addEventListener('click', colorTimeline, false);
 
-function getColorInfo(){
-  let pixelInfo;
-  console.log('You clicked color button!');
-  pixelInfo = ctx.getImageData(0,0,20,canvasEl.height);
-  debugger
+function colorTimeline(){
+  let aud1Dur = soundObj['audio1'].duration();
+  let aud2Dur = soundObj['audio2'].duration();
+  let aud3Dur = soundObj['audio3'].duration();
+
+  let shortestDur = Math.min(aud1Dur, aud2Dur, aud3Dur);
+  let millies = (shortestDur / canvasEl.width) * 1000;
+
+  setInt = window.setInterval(()=>{moveHead()}, millies);
+    function moveHead(){
+        current_x++;
+        getColorInfo(current_x);
+        // needs to account for painting
+        ctx.drawImage(currentImg,0,0,1000,600);
+        ctx.fillStyle = "rgba(255, 255, 255, 0.5)";
+        ctx.fillRect(current_x, 0, 2, 600);
+        if(current_x >= canvasEl.width){
+          stopInterval();
+        }
+    }
+    playAll();
 }
 
-const imageLoader = document.getElementById('imageLoader');
-imageLoader.addEventListener('change', handleImage, false);
+let redData = [];
+let greenData = [];
+let blueData = [];
+let alphaData = [];
+let pixelInfo;
+let redSum;
+let greenSum;
+let blueSum;
+let alphaSum;
+
+function getColorInfo(x_coord){
+  pixelInfo = ctx.getImageData(x_coord+2,0,1,canvasEl.height);
+  redData = [];
+  greenData = [];
+  blueData = [];
+  alphaData = [];
+
+  for (let i = 0; i < pixelInfo.data.length; i = i + 4) {
+    redData.push(pixelInfo.data[i]);
+  }
+
+  for (let i = 1; i < pixelInfo.data.length; i = i + 4) {
+    greenData.push(pixelInfo.data[i]);
+  }
+
+  for (let i = 2; i < pixelInfo.data.length; i = i + 4) {
+    blueData.push(pixelInfo.data[i]);
+  }
+
+  for (let i = 3; i < pixelInfo.data.length; i = i + 4) {
+    alphaData.push(pixelInfo.data[i]);
+  }
+
+  redSum = redData.reduce((acc, val) => {
+    return acc + val;
+  }, 0);
+  greenSum = greenData.reduce((acc, val) => {
+    return acc + val;
+  }, 0);
+  blueSum = blueData.reduce((acc, val) => {
+    return acc + val;
+  }, 0);
+  alphaSum = alphaData.reduce((acc, val) => {
+    return acc + val;
+  }, 0);
+
+  let max = canvasEl.height * 255;
+
+  soundObj['audio1'].volume(redSum/max);
+  soundObj['audio2'].volume(greenSum/max);
+  soundObj['audio3'].volume(blueSum/max);
+
+  // seems to work fine; could also do: element.setAttribute("style", "background-color: red;");
+  document.getElementById("volumeTest").innerHTML = redSum/max;
+}
+
+const stopIntervalButton = document.getElementById('stopIntervalButton');
+stopIntervalButton.addEventListener('click', stopInterval, false);
+
+function stopInterval(){
+  stopAll();
+  window.clearInterval(setInt);
+  // needs to account for painting
+  ctx.drawImage(currentImg,0,0,1000,600);
+  current_x = 0;
+  console.log('Interval stopped!')
+}
 
 const audioLoaders = document.getElementsByClassName('audioLoader');
 Array.prototype.forEach.call(audioLoaders, (loader) =>{
   loader.addEventListener('change', handleAudio, false);
 });
 
-const playButton = document.getElementById('playButton');
-playButton.addEventListener('click', playAll, false);
-
-const stopButton = document.getElementById('stopButton');
-stopButton.addEventListener('click', stopAll, false);
-
 const pauseButton = document.getElementById('pauseButton');
-pauseButton.addEventListener('click', pauseAll, false);
+pauseButton.addEventListener('click', pauseInterval, false);
 
-const volumeNumber = document.getElementById('volumeNumber');
-volumeNumber.addEventListener('change', changeVolume, false);
-
-function changeVolume(e){
-  // right now just changes the first file
-  // could get this to work for any of the three
-  console.log('You changed the volume!');
-  soundObj['audio1'].volume(e.target.value);
+function pauseInterval(){
+  pauseAll();
+  window.clearInterval(setInt);
+  console.log('Interval paused!')
 }
 
 function playAll(){
-  console.log('you clicked PLAY');
   soundObj['audio1'].play();
   soundObj['audio2'].play();
   soundObj['audio3'].play();
 }
 
 function stopAll(){
-  console.log('you clicked STOP');
   soundObj['audio1'].stop();
   soundObj['audio2'].stop();
   soundObj['audio3'].stop();
 }
 
 function pauseAll(){
-  console.log('you clicked PAUSE');
   soundObj['audio1'].pause();
   soundObj['audio2'].pause();
   soundObj['audio3'].pause();
@@ -152,17 +163,34 @@ function handleAudio(e){
     reader.readAsDataURL(e.target.files[0]);
 }
 
+const imageLoader = document.getElementById('imageLoader');
+imageLoader.addEventListener('change', handleImage, false);
 
 function handleImage(e){
     let reader = new FileReader();
     reader.onload = function(event){
         let img = new Image();
         img.onload = function(){
-            canvasEl.width = img.width / 2;
-            canvasEl.height = img.height / 2;
-            ctx.drawImage(img,0,0,img.width / 2,img.height / 2);
+            // canvasEl.width = img.width / 2;
+            // canvasEl.height = img.height / 2;
+            canvasEl.width = 1000;
+            canvasEl.height = 600;
+            currentImg = img;
+            ctx.drawImage(img,0,0,1000,600);
         }
         img.src = event.target.result;
     }
     reader.readAsDataURL(e.target.files[0]);
 }
+
+// start square in top left
+ctx.fillStyle = 'orange';
+// ctx.strokeStyle = 'blue';
+ctx.fillRect(0, 0, 3, 3);
+
+// this does seem to work to load images in
+// var htmlImg = document.getElementById("htmlImg");
+// htmlImg.onload = function ()
+// {
+//   ctx.drawImage(htmlImg, 0, 0);
+// }
